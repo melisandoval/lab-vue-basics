d9bc7de4d657b7ff8a5d49dbbf6170d467ae2c59
melisand <melisandoval@live.com.ar>
2022-12-08 21:25:16 +0100

    add iteration 3

--- lab-vue-basics/src/App.vue
+++ lab-vue-basics/src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <Navbar />
+  <main>
+    <p v-if="show">This is a conditional element</p>
+  </main>
   <Footer />
 </template>
 
 <script setup>
 import Navbar from "./components/Navbar.vue";
 import Footer from "./components/Footer.vue";
+import { ref } from "vue";
+
+const show = ref(true);
 </script>
 
 <style scoped></style>
